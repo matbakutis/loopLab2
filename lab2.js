910ed10adbafb58abd250aa82cde051968c1faba
matbakutis <31048002+matbakutis@users.noreply.github.com>
2017-09-13 12:29:30 -0500

    Added bonus loop

--- lab2.js
+++ lab2.js
@@ -1,15 +1,15 @@
-for (i = 50; i >= 20; i--){
+for (let i = 50; i >= 20; i--){
 	if ((i % 3) === 0){
 		console.log(i);
 	};
 };
 
-for (i = 0; i >= 0; i++){
+for (let i = 0; i >= 0; i++){
 	console.log("TO INFINITY AND BEYOND");
 };
 
 const classmates = ["Bridjet", "Nathan", "Matias", "Mike", "Jason", "Reuben", "Ben", "Lane", "John", "Chris", "Terry", "Ann", "Katelyn", "Naomi", "Jeff"];
-for (i = 0; i < classmates.length; i++){
+for (let i = 0; i < classmates.length; i++){
 	if (i % 2 === 0){
 		console.log(classmates[i]);		
 	};
@@ -22,8 +22,18 @@ const menu = [{name: "pizza", glutenFree: false},
 				{name: "chicken", glutenFree: true},
 				{name: "cheeseburger", glutenFree: false}];
 const glutenFreeMenu = [];
-for (i = 0; i < menu.length; i++){
+for (let i = 0; i < menu.length; i++){
 	if (menu[i].glutenFree === true){
 		glutenFreeMenu.push(menu[i]);
+	};
+};
+
+const league = [{name: "NFL", players: [{name: "Tom Brady", goat: false}, {name: "Walter Payton", goat: true}]}, {name: "NBA", players: [{name: "Michael Jordan", goat: true}, {name: "Lebron James", goat: false}]},{name: "MLB", players: [{name: "Babe Ruth", goat: true}, {name: "Alex Rodriguez", goat: false}]}];
+const goats = [];
+for (let i = 0; i < league.length; i++){
+	for (let a = 0; a < league[i].players.length; a++){
+		if (league[i].players[a].goat === true){
+			goats.push(league[i].players[a]);
+		};
 	};
 };
